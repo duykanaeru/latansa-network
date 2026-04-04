bc02a55d66afbc590627f831a98fe9641c0c6177
Duy <adeargam7@gmail.com>
2026-04-04 21:54:18 +0700

    block admin on vercel, use admin.latansa.net instead

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    allowedHosts: ["admin.latansa.net"],
     host: "::",
     port: 8080,
   },
